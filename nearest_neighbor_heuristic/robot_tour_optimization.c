eaf69d2c352a3c8fcbc1177a7a25206498e5b307
Phillip Julien <pjulien93@gmail.com>
2025-01-31 19:58:29 -0500

    Fri Jan 31 07:58:29 PM EST 2025

---
+++ nearest_neighbor_heuristic/robot_tour_optimization.c
@@ -0,0 +1,10 @@
+>   NearestNeighbor($P$)
+>       Pick and visit an initial point `$p_0$` from $P$
+>       `$p = p_0$`
+>       $i = 0$
+>       While there are still unvisited points
+>           $i = i + 1$
+>           Select `$p_i$` to be the closest unvisited point to `$p_i−1$`
+>           Visit `$p_i$`
+>       Return to `$p_0$` from `$p_n−1$`
+> (The Algorithm Design Manual; Chapter 1 Introduction to Algorithm Design; pp. 5-6; Steven S. Skiena; 2020)
