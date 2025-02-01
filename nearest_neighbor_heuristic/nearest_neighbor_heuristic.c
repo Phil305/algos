f0cff065a1a4b29a998dbb6a88b57adc1d2b8477
Phillip Julien <pjulien93@gmail.com>
2025-02-01 09:44:07 -0500

    Sat Feb  1 09:44:07 AM EST 2025

--- nearest_neighbor_heuristic/nearest_neighbor_heuristic.c
+++ nearest_neighbor_heuristic/nearest_neighbor_heuristic.c
@@ -11,23 +11,24 @@ void tearDown(void)/* {{{ */
 /* }}} */
 
 static void test_GivenXYZ_DoABC(void) {
-    /*
-     *  NearestNeighbor($P$)
-     *      Pick and visit an initial point `$p_0$` from $P$
-     *      `$p = p_0$`
-     *      $i = 0$
-     *      While there are still unvisited points
-     *          $i = i + 1$
-     *          Select `$p_i$` to be the closest unvisited point to `$p_i−1$`
-     *          Visit `$p_i$`
-     *      Return to `$p_0$` from `$p_n−1$`
-     * (The Algorithm Design Manual; Chapter 1 Introduction to Algorithm Design; pp. 5-6; Steven S. Skiena; 2020)
-     */
     int expected[] = {-21,-5,-1,0,1, 3, 11};
-    int array[] = {0,1,-21,-5, 11,-1, 3};
+
+    //  NearestNeighbor($P$)
+    //      Pick and visit an initial point `$p_0$` from $P$
+    int points[] = {0,1,-21,-5, 11,-1, 3};
+    int init_point = points[0];
+    int i = 0;
+    //      While there are still unvisited points
+    //          $i = i + 1$
+    while (unvisited_points(points)) {
+    //          Select `$p_i$` to be the closest unvisited point to `$p_i−1$`
+    //          Visit `$p_i$`
+    //      Return to `$p_0$` from `$p_n−1$`
+    // (The Algorithm Design Manual; Chapter 1 Introduction to Algorithm Design; pp. 5-6; Steven S. Skiena; 2020)
+    }
+
     // [Unity Assertions Reference](https://github.com/ThrowTheSwitch/Unity/tree/v2.6.1/docs/UnityAssertionsReference.md)
-    raise(SIGINT);
-    TEST_ASSERT_EQUAL_INT_ARRAY(/*expected=*/expected, /*actual=*/array, /*num_elements=*/sizeof(expected));
+    TEST_ASSERT_EQUAL_INT_ARRAY(/*expected=*/expected, /*actual=*/array, /*num_elements=*/sizeof(expected)/sizeof(int));
 }
 
 int main(void) {
