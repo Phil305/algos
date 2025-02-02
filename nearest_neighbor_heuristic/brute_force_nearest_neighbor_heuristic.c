5384ed8dc5b91fa0325740996f2e2a2150ecb8a3
Phillip Julien <pjulien93@gmail.com>
2025-02-01 20:37:09 -0500

    Sat Feb  1 08:37:09 PM EST 2025

--- nearest_neighbor_heuristic/brute_force_nearest_neighbor_heuristic.c
+++ nearest_neighbor_heuristic/brute_force_nearest_neighbor_heuristic.c
@@ -1,3 +1,7 @@
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wunused-variable"
+
 #include <signal.h>
 #include <stdbool.h>
 
@@ -10,17 +14,33 @@ void tearDown(void)/* {{{ */
 {}
 /* }}} */
 
+static bool unvisited_points(int *points[], int num_points) {
+    for (int i = 0; i<num_points; i++) {
+        if (!points[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 static void test_GivenXYZ_DoABC(void) {
     int expected[] = {-21,-5,-1,0,1, 3, 11};
+    int actual[] = {1,2};
 
     //  NearestNeighbor($P$)
     //      Pick and visit an initial point `$p_0$` from $P$
-    int points[] = {0,1,-21,-5, 11,-1, 3};
-    int init_point = points[0];
+    int points_[] = {0,1,-21,-5, 11,-1, 3};
+    int *points[sizeof(points_)/sizeof(int)];
+    for (unsigned long i=0; i<sizeof(points_)/sizeof(int); i++) {
+        points[i] = &points_[i];
+    }
+    int init_point = *points[0];
     int i = 0;
     //      While there are still unvisited points
     //          $i = i + 1$
-    while (unvisited_points(points)) {
+    while (unvisited_points(points, sizeof(points_)/sizeof(int))) {
+        if (i==7) {break;}
+        i++;
     //          Select `$p_i$` to be the closest unvisited point to `$p_i−1$`
     //          Visit `$p_i$`
     //      Return to `$p_0$` from `$p_n−1$`
@@ -28,7 +48,7 @@ static void test_GivenXYZ_DoABC(void) {
     }
 
     // [Unity Assertions Reference](https://github.com/ThrowTheSwitch/Unity/tree/v2.6.1/docs/UnityAssertionsReference.md)
-    TEST_ASSERT_EQUAL_INT_ARRAY(/*expected=*/expected, /*actual=*/array, /*num_elements=*/sizeof(expected)/sizeof(int));
+    TEST_ASSERT_EQUAL_INT_ARRAY(/*expected=*/expected, /*actual=*/actual, /*num_elements=*/sizeof(expected)/sizeof(int));
 }
 
 int main(void) {
@@ -37,3 +57,4 @@ int main(void) {
     RUN_TEST(/*UnityDefaultTestRun=*/test_GivenXYZ_DoABC);
     return UNITY_END();
 }
+#pragma clang diagnostic pop
