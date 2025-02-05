9340bb228d19bdcc5084984d3d1bcb7003dffe34
Phillip Julien <pjulien93@gmail.com>
2025-02-04 19:31:31 -0500

    Tue Feb  4 07:31:31 PM EST 2025

--- nearest_neighbor_heuristic/brute_force_nearest_neighbor_heuristic.c
+++ nearest_neighbor_heuristic/brute_force_nearest_neighbor_heuristic.c
@@ -3,6 +3,7 @@
 #pragma clang diagnostic ignored "-Wunused-variable"
 
 #include <signal.h>
+#include <assert.h>
 #include <stdbool.h>
 
 #include "unity.h"
@@ -14,37 +15,44 @@ void tearDown(void) /* {{{ */
 {}
 /* }}} */
 
-static bool unvisited_points(int *points[], int num_points) {
+static bool unvisited_points(int *points[], int num_points) { /* {{{ */
     for (int i = 0; i < num_points; i++) {
-        if (!points[i]) {
-            return false;
+        if (points[i] == NULL) {
+            return true;
         }
     }
-    return true;
+    return false;
+}
+/* }}} */
+static int closest_unvisited(int init_point_idx, int **points, int num_points){
+    assert(points[init_point_idx]);
+    int init_point = *points[init_point_idx];
+    return 0;
 }
 
 static void test_GivenXYZ_DoABC(void) {
     int expected[] = {-21, -5, -1, 0, 1, 3, 11};
     int actual[] = {1, 2};
 
+    int points[] = {0, 1, -21, -5, 11, -1, 3};
+    int *P[sizeof(points) / sizeof(int)];
+    for (unsigned long i = 0; i < sizeof(points) / sizeof(int); i++) {
+        P[i] = &points[i];
+    }
     //  NearestNeighbor($P$)
     //      Pick and visit an initial point `$p_0$` from $P$
-    int points_[] = {0, 1, -21, -5, 11, -1, 3};
-    int *points[sizeof(points_) / sizeof(int)];
-    for (unsigned long i = 0; i < sizeof(points_) / sizeof(int); i++) {
-        points[i] = &points_[i];
-    }
-    int init_point = *points[0];
+    int init_point = *P[0];
     int i = 0;
     //      While there are still unvisited points
-    //          $i = i + 1$
-    i++;
-    while (unvisited_points(points, sizeof(points_) / sizeof(int))) {
+    while (unvisited_points(/*points=*/P, sizeof(points) / sizeof(int))) {
+        //          $i = i + 1$
+        i++;
         if (i == 7) {
             break;
         }
         //          Select `$p_i$` to be the closest unvisited point to
-        closest_unvisited(init_point, points);
+        closest_unvisited(/*init_point_idx=*/i, P,
+                          /*num_points=*/sizeof(points) / sizeof(int));
         //          `$p_i−1$` Visit `$p_i$`
         //      Return to `$p_0$` from `$p_n−1$`
         // (The Algorithm Design Manual; Chapter 1 Introduction to
