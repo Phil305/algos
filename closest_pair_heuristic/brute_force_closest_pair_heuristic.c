ebdb9d4eea4a581aef8e8d046de03362c54c5833
Phillip Julien <pjulien93@gmail.com>
2025-03-16 16:02:03 -0400

    Sun Mar 16 04:02:03 PM EDT 2025

--- closest_pair_heuristic/brute_force_closest_pair_heuristic.c
+++ closest_pair_heuristic/brute_force_closest_pair_heuristic.c
@@ -1,19 +1,17 @@
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wcovered-switch-default"
-#pragma clang diagnostic ignored "-Wunsafe-buffer-usage"
-#pragma clang diagnostic ignored "-Wunused-parameter"
-#pragma clang diagnostic pop
-
+#include <limits.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-#include "unity.h"
-
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-function"
+#pragma clang diagnostic ignored "-Wmissing-prototypes"
 void setUp(void)
 {}
 
 void tearDown(void)
 {}
+#pragma clang diagnostic pop
 
 /* ClosestPair(P)
  *      Let n be the number of points in set P.
@@ -26,17 +24,14 @@ void tearDown(void)
  *
  * [source: algo design manual, p.7]
  */
-static void test_GivenXYZ_DoABC(void) {
-    // acts like a breakpoint when hit by GDB
-    //raise(SIGINT);
-
-    // [Unity Assertions Reference](https://github.com/ThrowTheSwitch/Unity/tree/v2.6.1/docs/UnityAssertionsReference.md)
-    TEST_ASSERT_EQUAL_INT(/*expected=*/true, /*actual=*/false);
-}
 
 int main(void) {
-    // [Unity - Getting Started](https://github.com/ThrowTheSwitch/Unity/blob/v2.6.1/docs/UnityGettingStartedGuide.md)
-    UNITY_BEGIN();
-    RUN_TEST(/*UnityDefaultTestRun=*/test_GivenXYZ_DoABC);
-    return UNITY_END();
+    int P[] = {0, -21, 11, -5, 3, -1, 1};
+    int d = INT_MAX;
+
+    for (unsigned long i=0; i < sizeof(P) / sizeof(int); i++) {
+        printf("%d\n", P[i]);
+    }
+
+    return 0;
 }
