d01c832eccfbb517835a052dce72edd05198e58f
Phillip Julien <pjulien93@gmail.com>
2025-03-20 10:20:45 -0400

    Thu Mar 20 10:20:45 AM EDT 2025

--- closest_pair_heuristic/brute_force_closest_pair_heuristic.c
+++ closest_pair_heuristic/brute_force_closest_pair_heuristic.c
@@ -23,33 +23,52 @@ void tearDown(void) {}
  * [source: algo design manual, p.7]
  */
 
-// static int dist(int n1, int n2) {
-//     return abs(n1 - n2);
-// }
+static int dist(int n1, int n2) {
+    return abs(n1 - n2);
+}
 
 static void endpoint_pairs(int ret[][2], int const endpoints[], int len) {
-    for (int i = 1; i < len; i++) {
-        ret[i - 1][0] = endpoints[i - 1];
-        ret[i - 1][1] = endpoints[i];
+    for (int i = 0; i < len; i++) {
+        int j = i;
+        if (i == 0) {
+            j = len;
+        }
+        ret[i][0] = endpoints[j - 1];
+        ret[i][1] = endpoints[i];
+    }
+}
+
+static void print_pairs(int pairs[][2], int const len) {
+    for (int i = 0; i < len; i++) {
+        printf("(%d, %d)\n", pairs[i][0], pairs[i][1]);
     }
 }
 
 int main(void) {
     int P[] = {0, -21, 11, -5, 3, -1, 1};
     int pairs[sizeof(P) / sizeof(int)][2] = {0};
+    /// int ordered_pairs[sizeof(P)/sizeof(int)] = {0};
+
     endpoint_pairs(/*ret=*/pairs, /*endpoints=*/P, /*len=*/sizeof(P) / sizeof(int));
+    print_pairs(pairs, /*len=*/sizeof(P) / sizeof(int));
 
-    for (unsigned long i = 0; i < (sizeof(P) / sizeof(int)) - 1; i++) {
-        printf("(%d, %d)\n", pairs[i][0], pairs[i][1]);
+    for (unsigned long i = 0; i < sizeof(P) / sizeof(int); i++) {
+        int d = INT_MAX;
+        int s = 0;
+        int t = 0;
+        for (unsigned long j = i; j < sizeof(P) / sizeof(int); j++) {
+            int s_ = pairs[j][0];
+            int t_ = pairs[j][1];
+            if (dist(s_, t_) <= d) {
+                s = s_;
+                t = t_;
+                d = dist(s_, t_);
+            } else {
+                continue;
+            }
+        }
+        printf("i=%lu; (%d, %d); d=%d\n",i, s, t, d);
     }
-    // int s = 0;
-    // int t = 0;
-
-    // for (unsigned long i = 1; i < sizeof(P) / sizeof(int); i++) {
-    //     int d = INT_MAX;
-    //     s = P[i - 1];
-    //     t = P[i];
-    //     if (dist(s, t) <= d) {
 
     return 0;
 }
