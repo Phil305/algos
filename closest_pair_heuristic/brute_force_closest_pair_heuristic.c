965e8a3d08da205a0d5cbb78b92bcd56f81e10d1
Phillip Julien <pjulien93@gmail.com>
2025-03-22 21:36:39 -0400

    Sat Mar 22 09:36:39 PM EDT 2025

--- closest_pair_heuristic/brute_force_closest_pair_heuristic.c
+++ closest_pair_heuristic/brute_force_closest_pair_heuristic.c
@@ -23,52 +23,59 @@ void tearDown(void) {}
  * [source: algo design manual, p.7]
  */
 
-static int dist(int n1, int n2) {
-    return abs(n1 - n2);
-}
-
-static void endpoint_pairs(int ret[][2], int const endpoints[], int len) {
-    for (int i = 0; i < len; i++) {
-        int j = i;
-        if (i == 0) {
-            j = len;
+// static int dist(int n1, int n2) { /* {{{ */
+//     return abs(n1 - n2);
+// }
+/* }}} */
+static void distinct_vertex_chains(int ret[][2], int const endpoints[], int endpoints_len) { /* {{{ */
+    int offset = 0;
+    for (int i = 0; i < endpoints_len; i++) {
+        for (int j = i; j < endpoints_len - 1; j++) {
+            ret[offset][0] = endpoints[i];
+            ret[offset][1] = endpoints[j + 1];
+            offset += 1;
         }
-        ret[i][0] = endpoints[j - 1];
-        ret[i][1] = endpoints[i];
+        printf("%d\n", offset);
+        if (offset == 21) {
+            raise(SIGINT);
+        }
     }
 }
-
-static void print_pairs(int pairs[][2], int const len) {
-    for (int i = 0; i < len; i++) {
-        printf("(%d, %d)\n", pairs[i][0], pairs[i][1]);
-    }
+/* }}} */
+// static void print_pairs(int pairs[][2], int const len) { /* {{{ */
+//     for (int i = 0; i < len; i++) {
+//         for (int j = i; j < len; j++) {
+//             printf("(%d, %d)\n", pairs[j][0], pairs[j][1]);
+//         }
+//     }
+// }
+static int triangular_number(int height) {
+    return (height * (height + 1)) / 2;
 }
-
+/* }}} */
 int main(void) {
     int P[] = {0, -21, 11, -5, 3, -1, 1};
-    int pairs[sizeof(P) / sizeof(int)][2] = {0};
+    int pairs[triangular_number(/*height=*/sizeof(P) / sizeof(int))][2];
     /// int ordered_pairs[sizeof(P)/sizeof(int)] = {0};
 
-    endpoint_pairs(/*ret=*/pairs, /*endpoints=*/P, /*len=*/sizeof(P) / sizeof(int));
-    print_pairs(pairs, /*len=*/sizeof(P) / sizeof(int));
+    distinct_vertex_chains(/*ret=*/pairs, /*endpoints=*/P, /*endpoints_len=*/sizeof(P) / sizeof(int));
+    // print_pairs(pairs, /*len=*/sizeof(P) / sizeof(int));
 
-    for (unsigned long i = 0; i < sizeof(P) / sizeof(int); i++) {
-        int d = INT_MAX;
-        int s = 0;
-        int t = 0;
-        for (unsigned long j = i; j < sizeof(P) / sizeof(int); j++) {
-            int s_ = pairs[j][0];
-            int t_ = pairs[j][1];
-            if (dist(s_, t_) <= d) {
-                s = s_;
-                t = t_;
-                d = dist(s_, t_);
-            } else {
-                continue;
-            }
-        }
-        printf("i=%lu; (%d, %d); d=%d\n",i, s, t, d);
-    }
+    // for (unsigned long i = 1; i < sizeof(P) / sizeof(int); i++) {
+    //     int d = INT_MAX;
+    //     int s = 0;
+    //     int t = 0;
+    //     for (unsigned long j = i; j < sizeof(P) / sizeof(int); j++) {
+    //         int s_ = pairs[i][0];
+    //         int t_ = pairs[j][1];
+    //         if (dist(s_, t_) <= d) {
+    //             s = s_;
+    //             t = t_;
+    //             d = dist(s_, t_);
+    //         }
+    //         printf("j=%lu; (%d, %d); d=%d\n", j, s, t, d);
+    //     }
+    // }
 
     return 0;
 }
