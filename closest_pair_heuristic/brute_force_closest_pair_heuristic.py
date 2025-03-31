fa804a0e0de2249af680efe4e411e1ac717fb129
Phillip Julien <pjulien93@gmail.com>
2025-03-31 18:55:21 -0400

    Mon Mar 31 06:55:21 PM EDT 2025

--- closest_pair_heuristic/brute_force_closest_pair_heuristic.py
+++ closest_pair_heuristic/brute_force_closest_pair_heuristic.py
@@ -1,3 +1,8 @@
+# pyright: strict
+
+import logging
+import typing
+
 # ClosestPair(P)
 #      Let n be the number of points in set P.
 #      For i = 1 to n − 1 do
@@ -10,14 +15,57 @@
 # [source: algo design manual, p.7]
 
 
-def uniq_pairs(P):
+def configure_logging() -> None:# {{{
+    logging.basicConfig(
+        format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
+        datefmt="%Y-%m-%d:%H:%M:%S",
+        level=logging.DEBUG,
+        handlers=[logging.StreamHandler()],
+    )
+# }}}
+
+def uniq_pairs(P: typing.Sequence[int]) -> list[tuple[int, int]]:# {{{
     n = len(P)
-    ret = []
+    ret: list[tuple[int,int]] = []
     for i in range(1, n):
         for j in range(i, n):
             ret.append((P[i - 1], P[j]))
+    return ret
+# }}}
 
+def dist(s:int, t:int)->int:# {{{
+    return abs(s - t)
+# }}}
 
-for pair in uniq_pairs(P=[0, -21, 11, -5, 3, -1, 1]):
+configure_logging()
+
+P: list[int] = [0, -21, 11, -5, 3, -1, 1]
+n = len(P)
+
+closest_pairs: list[tuple[int,int]] = []
+connected_pairs: set[tuple[int,int]] = set()
+for i in range(1, n):
     d = float("inf")
-    # for k
+    sm = 0
+    tm = 0
+    for pair in uniq_pairs(P):
+        if pair in connected_pairs:
+            continue
+
+        s = pair[0]
+        t = pair[1]
+        if dist(s, t) <= d:
+            sm = s
+            tm = t
+            d = dist(s, t)
+    closest_pairs.append((sm, tm))
+    connected_pairs.add((sm,tm))
+    breakpoint()
+
+#path: list[int] = []
+
+# path_from_endpoints
+#for pair in connected_pairs:
+
+
+breakpoint()
