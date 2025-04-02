f900b86725d6b8c8d7780defe6054cb4cfd48da9
Phillip Julien <pjulien93@gmail.com>
2025-04-01 21:06:50 -0400

    Tue Apr  1 09:06:50 PM EDT 2025

--- closest_pair_heuristic/brute_force_closest_pair_heuristic.py
+++ closest_pair_heuristic/brute_force_closest_pair_heuristic.py
@@ -15,57 +15,95 @@ import typing
 # [source: algo design manual, p.7]
 
 
-def configure_logging() -> None:# {{{
+def configure_logging() -> None:  # {{{
     logging.basicConfig(
         format="%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
         datefmt="%Y-%m-%d:%H:%M:%S",
         level=logging.DEBUG,
         handlers=[logging.StreamHandler()],
     )
+
+
 # }}}
 
-def uniq_pairs(P: typing.Sequence[int]) -> list[tuple[int, int]]:# {{{
+
+def uniq_pairs(P: typing.Sequence[int]) -> list[tuple[int, int]]:  # {{{
     n = len(P)
-    ret: list[tuple[int,int]] = []
+    ret: list[tuple[int, int]] = []
     for i in range(1, n):
         for j in range(i, n):
             ret.append((P[i - 1], P[j]))
     return ret
+
+
 # }}}
 
-def dist(s:int, t:int)->int:# {{{
+
+def dist(s: int, t: int) -> int:  # {{{
     return abs(s - t)
+
+
 # }}}
 
-configure_logging()
 
-P: list[int] = [0, -21, 11, -5, 3, -1, 1]
-n = len(P)
+def closest_pairs(P: typing.Sequence[int], n: int):  # {{{
+    ret: list[tuple[int, int]] = []
+    connected_nodes: set[int] = set()
 
-closest_pairs: list[tuple[int,int]] = []
-connected_pairs: set[tuple[int,int]] = set()
-for i in range(1, n):
-    d = float("inf")
-    sm = 0
-    tm = 0
-    for pair in uniq_pairs(P):
-        if pair in connected_pairs:
+    for _ in range(1, n):
+
+        d = float("inf")
+        sm = 0
+        tm = 0
+
+        for pair in uniq_pairs(P):
+
+            if pair[0] in connected_nodes and pair[1] in connected_nodes:
+                continue
+
+            s = pair[0]
+            t = pair[1]
+
+            if dist(s, t) <= d:
+                sm = s
+                tm = t
+                d = dist(s, t)
+
+        connected_nodes.update((sm, tm))
+        ret.append((min(sm, tm), max(sm, tm)))
+
+    return ret
+
+
+# }}}
+
+
+def print_cycle(edges: typing.Sequence[tuple[int, int]]):  # {{{
+    cycle: list[int] = []
+
+    for pair in sorted(edges):
+
+        if not cycle:
+            cycle.extend(pair)
             continue
 
-        s = pair[0]
-        t = pair[1]
-        if dist(s, t) <= d:
-            sm = s
-            tm = t
-            d = dist(s, t)
-    closest_pairs.append((sm, tm))
-    connected_pairs.add((sm,tm))
-    breakpoint()
+        assert cycle[-1] == pair[0]
 
-#path: list[int] = []
+        cycle.append(pair[1])
 
-# path_from_endpoints
-#for pair in connected_pairs:
+    cycle.append(cycle[0])
+
+    print(cycle)
 
 
-breakpoint()
+# }}}
+
+
+def main():
+    configure_logging()
+    P: list[int] = [0, -21, 11, -5, 3, -1, 1]
+    print_cycle(edges=closest_pairs(P, n=len(P)))
+
+
+if __name__ == "__main__":
+    main()
