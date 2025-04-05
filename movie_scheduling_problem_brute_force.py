65fe36bc9478cb61af89976a7c6c03409d018430
Phillip Julien <pjulien93@gmail.com>
2025-04-04 20:22:00 -0400

    Fri Apr  4 08:22:00 PM EDT 2025

---
+++ movie_scheduling_problem_brute_force.py
@@ -0,0 +1,4 @@
+# > Problem: Movie Scheduling Problem
+# > Input: A set I of n intervals on the line.
+# > Output: What is the largest subset of mutually non-overlapping intervals that can be selected from I?
+# > [p. 9 of Algo Design Manual]
