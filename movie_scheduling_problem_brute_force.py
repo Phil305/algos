2eaf7344050f910ead72c503daf388873bb7f0b6
Phillip Julien <pjulien93@gmail.com>
2025-04-05 21:03:35 -0400

    Sat Apr  5 09:03:35 PM EDT 2025

--- movie_scheduling_problem_brute_force.py
+++ movie_scheduling_problem_brute_force.py
@@ -2,3 +2,20 @@
 # > Input: A set I of n intervals on the line.
 # > Output: What is the largest subset of mutually non-overlapping intervals that can be selected from I?
 # > [p. 9 of Algo Design Manual]
+
+# Schedule(I)
+#   max_sum_intervals = 0
+#   For each subset of non overlapping intervals I_i of I from non_overlapping(I)
+#       if sum_intervals(I_i) > max_sum_intervals then max_sum_intervals = sum_intervals(I_i) and schedule = I_i
+
+I = {
+    Tarjan_of_the_Jungle,
+    The_Four_Volume_Problem,
+    The_Presidents_Algorist,
+    Steiners_Tree,
+    Process_Terminated,
+    Halting_State,
+    Programming_Challenges,
+    Discrete_Mathematics,
+    Calculated_Bets,
+}
